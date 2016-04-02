568399a1d8a0d7f42c87180aa12d549c3b933a23
SatyaMutyala <satyanarayana.mutyala@hotmail.com>
2016-04-01 23:46:14 -0400

    Intial Commit

---
+++ TestFirstMVCApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestFirstMVCApp.Startup))]
+namespace TestFirstMVCApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
